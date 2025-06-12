097cce20bdb4f4caa0a8aa5412e64b3e666fed35
AbdinasirM <aabdi5520@gmail.com>
2025-06-11 23:17:32 -0500

    set up of the  Thalio App

---
+++ ThalioApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ThalioApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
